5b1a850467c64140405e3c6071a2938186077c34
Jordan Noe <jordanvinnoe@gmail.com>
2020-05-19 14:05:06 -0400

    added get routes

--- server.js
+++ server.js
@@ -11,5 +11,16 @@ app.use(express.json());
 //routes for getting and posting
 
 //routes for displaying html
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
 
 //ajax call to express server
