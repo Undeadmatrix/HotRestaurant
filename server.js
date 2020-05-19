53039ca1f06e8bdb663ce3d3bb06b9bf178005b2
Doris Castro <dorisgeo12@gmail.com>
2020-05-19 13:44:44 -0400

    vaiables for holding response

---
+++ server.js
@@ -0,0 +1,25 @@
+//create a basic server
+
+// create a set of variables for holding the reservation
+var reservation = [
+    {
+        routeName: "johnDoe",
+        name:"John Doe",
+        phone: "123-555-5555",
+        email: "e@host.com",
+        specialRequest: "no cheese"
+
+    }
+]
+
+// create a set of variables for holding wait list
+var waitList = [
+    {
+        routeName: "janeDoe",
+        name:"Jan Doe",
+        phone: "123-555-4444",
+        email: "a@host.com",
+        specialRequest: " "
+
+    }
+]
